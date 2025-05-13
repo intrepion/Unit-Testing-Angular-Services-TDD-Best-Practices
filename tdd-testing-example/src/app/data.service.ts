c985ecb301be1190949b2a03585dd9c4c70213e0
Oliver Forral <intrepion@gmail.com>
2025-05-12 20:51:47 -0600

    Implement Service

--- tdd-testing-example/src/app/data.service.ts
+++ tdd-testing-example/src/app/data.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@angular/core';
+   // data.service.ts
+   import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService {
-
-  constructor() { }
-}
+   @Injectable({
+     providedIn: 'root'
+   })
+   export class DataService {
+     getData(): string {
+       return 'Hello, World!';
+     }
+   }
