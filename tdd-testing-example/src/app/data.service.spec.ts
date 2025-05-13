078cd9653037854eb730f32272e4a845cc4cdc38
Oliver Forral <intrepion@gmail.com>
2025-05-12 20:51:08 -0600

    Write Test First

--- tdd-testing-example/src/app/data.service.spec.ts
+++ tdd-testing-example/src/app/data.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+// data.service.spec.ts
+   import { TestBed } from '@angular/core/testing';
+   import { DataService } from './data.service';
 
-import { DataService } from './data.service';
+   describe('DataService', () => {
+     let service: DataService;
+     beforeEach(() => {
+       TestBed.configureTestingModule({});
+       service = TestBed.inject(DataService);
+     });
 
-describe('DataService', () => {
-  let service: DataService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+     it('should return data', () => {
+       expect(service.getData()).toBeTruthy();
+     });
+   });
+   
